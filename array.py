b8eb875364ff43b5077d745e661afbb832477256
monikaupadhyay29 <96279474+monikaupadhyay29@users.noreply.github.com>
2021-12-31 17:54:12 +0530

    commit

---
+++ array.py
@@ -0,0 +1,50 @@
+# arr=[1,3,5,7,9]
+# print(arr[0])
+# print(arr[1])
+# print(arr[2])
+
+# import array
+# arr=array.array('i', [1, 3, 5, 7, 9])
+# arr.append(11)
+# print(arr)
+
+# arr=array.array('i', [1, 3, 5, 3, 7, 1, 9, 3]) 
+# rev=arr.reverse()
+# print(arr)
+
+
+# arr=array.array('i', [1, 3, 5, 7, 9])
+# p=str(arr.itemsize)
+# print(p)
+
+
+# arr=array.array('i', [1, 3, 5, 3, 7, 9, 3])
+# print(arr.count(3))
+
+# arr=array.array('i', [1, 3, 5, 3, 7, 9, 3]) 
+# print(type(arr))
+# arr.extend(arr)
+# arr.remove(3)
+# p=list(arr)
+# print(p)
+
+# arr=array.array('i', [1, 3, 5, 7, 9])
+# arr.insert(1,4)
+# print(arr)
+
+# arr.remove(5)
+# print(arr)
+
+# arr1=[10,20,30,40,50]
+# for i in arr1:
+#     print(i)
+
+# print("Length of array:",len(arr1))
+
+
+
+# a=array.array('i',[1, 3, 5, 1, 3, 7, 9])
+# a=array.array('i',[2,4,2,6,4,8])
+# print(type(a)) 
+# s=set(a)
+# print(s) 
