0ccc46e8da97c62e20362954d40b8adff6f04523
monikaupadhyay29 <96279474+monikaupadhyay29@users.noreply.github.com>
2022-01-03 18:15:11 +0530

    first commit

---
+++ module.py
@@ -0,0 +1,13 @@
+from inheritance import calculation3
+
+obj=calculation3()
+print(obj.addition(5,6))
+
+print(obj.simple_interest(50,4,5))
+
+
+
+from function import myfun
+
+d=myfun(3,2)
+print(d)
